709d09d15a60cccbd68f4d9c85a884c8b17f87e9
Gabrielacpj <111399616+Gabrielacpj@users.noreply.github.com>
2023-10-17 07:44:32 -0300

    Update Xpert.js

--- Xpert.js
+++ Xpert.js
@@ -10,7 +10,7 @@ while (chute !=numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto}`);
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
    if (chute > numeroSecreto) {
         alert(`O número secreto é menor que ${chute}`);
